9e38d00c0f4af3e4548e2ed1085f09ff50c04a3c
louislaugier <l.laugier@protonmail.com>
2025-01-01 22:09:46 +0100

    --wip-- [skip ci]

--- frontend/src/lib/domCleaner.ts
+++ frontend/src/lib/domCleaner.ts
@@ -1,28 +1,15 @@
 export function initializeDOMCleaner(): void {
   const selectors = [
-    '.side-side-panel__header__bottom',
-    '.filter-manager>div:first-child',
-    '.filter-manager>div:nth-child(2)',
-    '.filter-manager>div:nth-child(3)',
-    '.layer-manager-header>button',
-    '.side-panel__top__actions',
-    '.side-panel__panel-header.side-side-panel__header.side-panel__panel-header',
-    '.sc-cVzyXs',
     '.bottom-widget__y-axis',
     '.playback-controls',
     '.animation-control__time-display__bottom',
     '.maplibre-attribution-container',
-    '.sc-ikkxIA.fOZEjb:nth-child(2)',
     '.map-control',
-    '.data-source-selector.side-panel-section.data-source-selector',
-    '.side-panel-section>div:nth-child(7)',
-    '.side-panel-panel__label',
-    '.edit-feature.toolbar-item.edit-feature',
-    '.panel--header__action',
     '.select-geometry',
     '.popover-arrow-left',
     '.popover-pin',
-    '.map-popover__layer-info>div:last-child'
+    '.map-popover__layer-info>div:last-child',
+    '.edit-feature.toolbar-item.edit-feature'
   ];
 
   function removeHiddenElements() {
