08680ad1c15ebc47076141c890630af339f91403
louislaugier <l.laugier@protonmail.com>
2025-03-16 02:12:09 +0100

    --wip-- [skip ci]

--- api/crons/tournaments/signup_urls.go
+++ api/crons/tournaments/signup_urls.go
@@ -2,12 +2,28 @@ package tournaments
 
 import (
 	"log"
+	"time"
 	"tournois-tt/api/pkg/utils"
 )
 
 func RefreshSignupURLs() {
-	lastSeasonStart, _ := utils.GetLastFinishedSeason()
-	if err := refreshGeocoding(&lastSeasonStart, nil); err != nil {
+	currentSeasonStart, _ := utils.GetCurrentSeason()
+	if err := refreshSignupURLs(&currentSeasonStart, nil); err != nil {
 		log.Printf("Warning: Failed to refresh tournament geocoding data: %v", err)
 	}
+}
+
+func refreshSignupURLs(startDateAfter, startDateBefore *time.Time) error {
+	// get tournaments from cache
+
+	// for each, if signup url not empty
+	// check hello asso with tournament name, tournament club, tournament city
+
+	// if signup url not found, if !siteExistenceChecked, try to get site url
+	// if site url found, check if tournament signup link available on site
+	// if signup url found, add to json
+
+	// if signup url not found, if pdf rules not checked, if has pdf rules, check pdf rules
+	// if signup url found, add to json
+	return nil
 }
